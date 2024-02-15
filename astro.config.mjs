07083a151c8dbd27191aed6d97ab58fe284957ec
jeremiah-carlson <87202597+jeremiah-carlson@users.noreply.github.com>
2024-02-15 01:33:51 -0500

    Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pondscrum.github.io',
+  base: '/unix-ts',
   integrations: [svelte(), tailwind()]
 });
